53db3f6003582cee2cf8f5d2a0f79649f647ced0
Ferrypjohar <ferryjohar.12@gmail.com>
2026-05-06 07:53:31 +0700

    Membuat package viewmodel

--- app/src/main/java/com/example/splitku/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/splitku/viewmodel/AuthViewModel.kt
@@ -1,12 +1,10 @@
 package com.example.splitku.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore // Pastikan ini ada
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class AuthViewModel : ViewModel() {
     private val auth = FirebaseAuth.getInstance()
