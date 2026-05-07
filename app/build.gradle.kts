a579f06b36cb9aac4fcc219a013e7beae29a7bf8
amalianisa0108 <nisaamalia0108@gmail.com>
2026-05-07 16:20:52 +0700

    menambahkan dependensi navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
