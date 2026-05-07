d7d296a940d72a16e0b77367018f634e9a615077
nandaherni19 <nandahernipramudyaningtiyas@gmail.com>
2026-05-07 22:12:56 +0700

    menambahkan dependency Room QR generator dan lifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,8 @@ dependencies {
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // dependency untuk qr code
+    implementation("com.google.zxing:core:3.5.3")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
